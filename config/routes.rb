388816019ecf99099cc5b251c6d57c94ef8e4e72
lisawilliams <lisa@placeblogger.com>
2017-06-01 09:31:51 -0400

    adding routes and migration files

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
+  get '/books', to: 'books#index'
+  get '/book/:id', to: 'books#show'
 
+  resources :books, except: [:new, :edit]
   resources :users, only: [:index, :show]
   resources :examples, except: [:new, :edit]
 end
